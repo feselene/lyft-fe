322b19b5b18bdda058e08f487d2cdb2684698c24
feselene <vaporflyb@gmail.com>
2024-10-23 16:29:41 -0400

    Step 1: build.gradle.kts add android plugins and ./gradlew clean command

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
-repositories {
-    mavenCentral()
+allprojects {
+    repositories {
+        google() // Google's Maven repository
+        mavenCentral() // Central repository for libraries
+    }
 }
 
 dependencies {
@@ -8,6 +11,18 @@ dependencies {
 
 plugins {
     `java-library`
+
+    // Plugin for Android applications
+    id("com.android.application") version "8.1.1" apply false
+
+    // Plugin for Android library modules (if needed in the future)
+    id("com.android.library") version "8.1.1" apply false
+
+    // Kotlin plugin for Android apps
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+
+    // Kotlin Kapt (for annotation processing, if you use libraries like Dagger, Hilt, etc.)
+    id("org.jetbrains.kotlin.kapt") version "1.9.10" apply false
 }
 
 java {
@@ -17,3 +32,7 @@ java {
 }
 
 version = "1.2.1"
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
